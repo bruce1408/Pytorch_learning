eb2ce5a1a0fb1df13a0777c38c41dbebc056aba7
bruce1408 <summer56567@163.com>
2020-07-09 00:24:44 +0800

    new update

--- 4-3.Bi-LSTM_Attention/Bi-LSTM_Attention_Torch.py
+++ 4-3.Bi-LSTM_Attention/Bi-LSTM_Attention_Torch.py
@@ -40,36 +40,39 @@ target_batch = torch.LongTensor(targets)
 
 class BiLSTM_Attention(nn.Module):
     def __init__(self):
+        """
+        双向LSTM 来实现 attention 机制
+        """
         super(BiLSTM_Attention, self).__init__()
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, n_hidden, bidirectional=True)
         self.out = nn.Linear(n_hidden * 2, num_classes)
 
-    # lstm_output : [batch_size, n_step, n_hidden * num_directions(=2)], F matrix
+    # lstm_output : [batch_size, n_step, n_hidden * num_directions(=2)]
     def attention_net(self, lstm_output, final_state):
+        # print(lstm_output.shape) = [6, 3, 10]
         # hidden : [batch_size, n_hidden * num_directions(=2), 1(=n_layer)]
         hidden = final_state.view(-1, n_hidden * 2, 1)
         attn_weights = torch.bmm(lstm_output, hidden).squeeze(2)  # attn_weights : [batch_size, n_step]
-        soft_attn_weights = F.softmax(attn_weights, 1)
+        soft_attn_weights = F.softmax(attn_weights, 1)  # [batch_size, n_step]=[6 * 3]
         # [batch_size, n_hidden * num_directions(=2), n_step] * [batch_size, n_step, 1] =
         # [batch_size, n_hidden * num_directions(=2), 1]
-        context = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
+        # print(lstm_output.transpose(1, 2).shape)  # [6, 10, 3] * [6, 3, 1]
+        context = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)  # [6, 10]
         return context, soft_attn_weights.data.numpy()  # context : [batch_size, n_hidden * num_directions(=2)]
 
     def forward(self, X):
         input = self.embedding(X)  # input : [batch_size, len_seq, embedding_dim]
         input = input.permute(1, 0, 2)  # input : [len_seq, batch_size, embedding_dim]
-
-        hidden_state = Variable(
-            torch.zeros(1 * 2, len(X), n_hidden))  # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
-        cell_state = Variable(
-            torch.zeros(1 * 2, len(X), n_hidden))  # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
-
+        # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+        hidden_state = torch.zeros(1 * 2, len(X), n_hidden)
+        # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+        cell_state = torch.zeros(1 * 2, len(X), n_hidden)
         # final_hidden_state, final_cell_state : [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
         output, (final_hidden_state, final_cell_state) = self.lstm(input, (hidden_state, cell_state))
-        output = output.permute(1, 0, 2)  # output : [batch_size, len_seq, n_hidden]
-        attn_output, attention = self.attention_net(output, final_hidden_state)
+        output = output.permute(1, 0, 2)  # output : [batch_size, len_seq, n_hidden*2]
+        attn_output, attention = self.attention_net(output, final_hidden_state)  # [6, 10], [6, 3]
         return self.out(attn_output), attention  # model : [batch_size, num_classes], attention : [batch_size, n_step]
 
 
@@ -90,7 +93,7 @@ for epoch in range(5000):
     optimizer.step()
 
 # Test
-test_text = 'sorry hate you'
+test_text = 'sorry for you'
 tests = [np.asarray([word_dict[n] for n in test_text.split()])]
 test_batch = Variable(torch.LongTensor(tests))
 
