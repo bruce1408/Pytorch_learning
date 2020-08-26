ccd141a5c48e0f05051ae5c6e3c65afb8c4f7fe0
bruce1408 <summer56567@163.com>
2020-08-26 16:28:07 +0800

    resnet 50 add new reference

--- CV/6-1.IMG/6_6_ResNet.py
+++ CV/6-1.IMG/6_6_ResNet.py
@@ -2,13 +2,21 @@ import torch
 import torch.nn as nn
 from torchsummary import summary
 """
-实现pytorch ResNet50网络结构
-https://zhuanlan.zhihu.com/p/28124810
+实现pytorch ResNet50网络结构,实现连接就是不用加卷积的情况
+reference: https://blog.csdn.net/Cheungleilei/article/details/103610799
 """
 
 
 class Bottleneck(nn.Module):
     def __init__(self, in_channel, out_channel, stride=1, downsampling=False, dim=4):
+        """
+        代码比tensorflow更加简洁和方便,
+        :param in_channel: 输入维度
+        :param out_channel: 输出维度
+        :param stride: 步长
+        :param downsampling: shortcut是否有卷积操作
+        :param dim: 维度参数
+        """
         super(Bottleneck, self).__init__()
         self.dim = dim
         self.downsampling = downsampling
@@ -51,8 +59,10 @@ class ResNet50(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(3, 2)
-        )
+        )  # output shape=[55, 55, 3]
+        # blocks = [3, 4, 6, 3]
 
+        # 4个block进行操作
         self.layer2 = self.make_layer(64, 64, blocks[0], stride=1)
         self.layer3 = self.make_layer(256, 128, blocks[1], stride=2)
         self.layer4 = self.make_layer(512, 256, blocks[1], stride=2)
@@ -63,10 +73,11 @@ class ResNet50(nn.Module):
 
     def make_layer(self, in_channel, out_channel, block_num, stride=1):
         layers = list()
-        layers.append(Bottleneck(in_channel, out_channel, stride, downsampling=True))
+        layers.append(Bottleneck(in_channel, out_channel, stride, downsampling=True))  # 第一个单元是带有卷积的shortcut
+
+        # 其余的部分不带卷积shortcut,直接把结果和输入进行叠加即可
         for i in range(1, block_num):
             layers.append(Bottleneck(out_channel*4, out_channel))
-        # return Bottleneck(in_channel, out_channels, stride, downsampling=True)
 
         return nn.Sequential(*layers)
 
