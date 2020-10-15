e807a695b9d2d9be2e511ccee722eac76d32b910
bruce1408 <summer56567@163.com>
2020-10-15 11:00:50 +0800

    lr

--- NLP/0-1.Basic/0_4_lr_adjust.py
+++ NLP/0-1.Basic/0_4_lr_adjust.py
@@ -18,3 +18,63 @@ for epoch in range(40):
     print(epoch, lrr.get_lr()[0])
     lrr.step()
     lre.step()
+
+
+def find_lr(data_loader, model, criterion, optimizer, device, init_value=1e-8, final_value=10., beta=0.98):
+    """
+    参考资料:https://blog.zhujian.life/posts/78a36c78.html
+    :param data_loader:
+    :param model:
+    :param criterion:
+    :param optimizer:
+    :param device:
+    :param init_value:
+    :param final_value:
+    :param beta:
+    :return:
+    """
+    num = len(data_loader) - 1
+    mult = (final_value / init_value) ** (1 / num)
+    lr = init_value
+    optimizer.param_groups[0]['lr'] = lr
+    avg_loss = 0.
+    best_loss = 0.
+    batch_num = 0
+    losses = []
+    log_lrs = []
+    for inputs, labels in data_loader:
+        batch_num += 1
+        print('{}: {}'.format(batch_num, lr))
+
+        # As before, get the loss for this mini-batch of inputs/outputs
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        # Compute the smoothed loss
+        avg_loss = beta * avg_loss + (1 - beta) * loss.item()
+        smoothed_loss = avg_loss / (1 - beta ** batch_num)
+
+        # Stop if the loss is exploding
+        if batch_num > 1 and smoothed_loss > 4 * best_loss:
+            return log_lrs, losses
+
+        # Record the best loss
+        if smoothed_loss < best_loss or batch_num == 1:
+            best_loss = smoothed_loss
+
+        # Store the values
+        losses.append(smoothed_loss)
+        log_lrs.append(math.log10(lr))
+
+        # Do the SGD step
+        loss.backward()
+        optimizer.step()
+
+        # Update the lr for the next step
+        lr *= mult
+        optimizer.param_groups[0]['lr'] = lr
+    return log_lrs, losses
