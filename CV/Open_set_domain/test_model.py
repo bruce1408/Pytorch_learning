d07c615b85520cbd36771981473d083c752e1ad8
bruce1408 <summer56567@163.com>
2020-10-17 14:55:36 +0800

    bceloss

---
+++ CV/Open_set_domain/test_model.py
@@ -0,0 +1,42 @@
+import argparse
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, ToTensor
+from tqdm import tqdm
+
+from data import MNISTM
+from models import Net
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def main(args):
+    dataset = MNISTM(train=False)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False,
+                            drop_last=False, num_workers=1, pin_memory=True)
+
+    model = Net().to(device)
+    model.load_state_dict(torch.load(args.MODEL_FILE))
+    model.eval()
+
+    total_accuracy = 0
+    with torch.no_grad():
+        for x, y_true in tqdm(dataloader, leave=False):
+            x, y_true = x.to(device), y_true.to(device)
+            y_pred = model(x)
+            total_accuracy += (y_pred.max(1)[1] == y_true).float().mean().item()
+    
+    mean_accuracy = total_accuracy / len(dataloader)
+    print(f'Accuracy on target data: {mean_accuracy:.4f}')
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description='Test a model on MNIST-M')
+    arg_parser.add_argument('--MODEL_FILE', default="./trained_models/revgrad.pt", help='A model in trained_models')
+    arg_parser.add_argument('--batch-size', type=int, default=256)
+    args = arg_parser.parse_args()
+    main(args)
--- NLP/0-1.Basic/0_13_gradReverse.py
+++ NLP/0-1.Basic/0_13_gradReverse.py
@@ -1,15 +1,95 @@
 import torch
+import torch.nn as nn
 from torch.autograd import Function
+"""
+https://blog.csdn.net/t20134297/article/details/107870906
+https://blog.csdn.net/yskyskyer123/article/details/94905856
+https://github.com/fungtion/DANN
+"""
+
+# 梯度不反转
+# x = torch.tensor([1., 2., 3.], requires_grad=True)
+# y = torch.tensor([4., 5., 6.], requires_grad=True)
+#
+# z = torch.pow(x, 2) + torch.pow(y, 2)
+# f = z + x + y
+# s = 6 * f.sum()
+# print(z)
+# print(f)
+# print(s)
+# s.backward()
+# print(x)
+# print(x.grad)
+
 
 x = torch.tensor([1., 2., 3.], requires_grad=True)
 y = torch.tensor([4., 5., 6.], requires_grad=True)
-
 z = torch.pow(x, 2) + torch.pow(y, 2)
-f = z + x + y
-s = 6 * f.sum()
 
-print(f)
-print(s)
+f = z + x + y
+
+
+class GRL(Function):
+    @staticmethod
+    def forward(self, input):
+        return input
+
+    @staticmethod
+    def backward(self, grad_output):
+        grad_input = grad_output.neg()
+        return grad_input
+
+
+class GradientReversalFunction(Function):
+    """
+    Gradient Reversal Layer from:
+    Unsupervised Domain Adaptation by Backpropagation (Ganin & Lempitsky, 2015)
+
+    Forward pass is the identity function. In the backward pass,
+    the upstream gradients are multiplied by -lambda (i.e. gradient is reversed)
+    """
+
+    @staticmethod
+    def forward(ctx, x, lambda_):
+        ctx.lambda_ = lambda_
+        return x.clone()
+
+    @staticmethod
+    def backward(ctx, grads):
+        lambda_ = ctx.lambda_
+        lambda_ = grads.new_tensor(lambda_)
+        dx = -lambda_ * grads
+        return dx, None
+
+
+class GradientReversal(torch.nn.Module):
+    def __init__(self, lambda_=1):
+        super(GradientReversal, self).__init__()
+        self.lambda_ = lambda_
+
+    def forward(self, x):
+        return GradientReversalFunction.apply(x, self.lambda_)
+
+
+# 第一种方法
+Grl = GRL()
+s = 6 * f.sum()
+s = Grl.apply(s)
+print('s ', s)
 s.backward()
-print(x)
-print(x.grad)
+print('x ', x)
+print('x grad: ', x.grad)
+
+
+# 第二种方式
+# discritor = nn.Sequential(
+#     GradientReversal()
+# )
+# s = 6 * f.sum()
+# s = discritor(s)
+# s.backward()
+# print(x)
+# print(x.grad)
+
+
+
