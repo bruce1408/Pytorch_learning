758c729eb6b21a8e99f7abec2c5849b403ab3315
bruce1408 <summer56567@163.com>
2022-06-23 01:23:07 +0800

    merge new code adjust cnn part

--- NLP/1-0.CNN/1_0_0_CNN_Sent_Polarity.py
+++ NLP/1-0.CNN/1_0_0_CNN_Sent_Polarity.py
@@ -1,16 +1,16 @@
-# Defined in Section 4.6.4 and 4.6.5
-
+import sys
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 from collections import defaultdict
-import sys
-from vocab import Vocab
-from utils import load_sentence_polarity
+sys.path.append("../")
+from tools.vocab import Vocab
+from tools.utils import load_sentence_polarity
 
 
-class BowDataset(Dataset):
+class CnnDataset(Dataset):
     def __init__(self, data):
         self.data = data
 
@@ -24,29 +24,29 @@ class BowDataset(Dataset):
 def collate_fn(examples):
     inputs = [torch.tensor(ex[0]) for ex in examples]
     targets = torch.tensor([ex[1] for ex in examples], dtype=torch.long)
-    offsets = [0] + [i.shape[0] for i in inputs]
-    offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
-    inputs = torch.cat(inputs)
-    return inputs, offsets, targets
+    # 对batch内的样本进行padding，使其具有相同长度
+    inputs = pad_sequence(inputs, batch_first=True)
+    return inputs, targets
 
 
-class MLP(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_class):
-        super(MLP, self).__init__()
-        self.embedding = nn.EmbeddingBag(vocab_size, embedding_dim)
-        self.linear1 = nn.Linear(embedding_dim, hidden_dim)
+class CNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, filter_size, num_filter, num_class):
+        super(CNN, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.conv1d = nn.Conv1d(embedding_dim, num_filter, filter_size, padding=1)
         self.activate = F.relu
-        self.linear2 = nn.Linear(hidden_dim, num_class)
+        self.linear = nn.Linear(num_filter, num_class)
 
-    def forward(self, inputs, offsets):
-        embedding = self.embedding(inputs, offsets)
-        hidden = self.activate(self.linear1(embedding))
-        outputs = self.linear2(hidden)
+    def forward(self, inputs):
+        embedding = self.embedding(inputs)
+        convolution = self.activate(self.conv1d(embedding.permute(0, 2, 1)))
+        pooling = F.max_pool1d(convolution, kernel_size=convolution.shape[2])
+        outputs = self.linear(pooling.squeeze(dim=2))
         log_probs = F.log_softmax(outputs, dim=1)
         return log_probs
 
 
-# tqdm是一个Python模块，能以进度条的方式显示迭代的进度
+# tqdm是一个Pyth模块，能以进度条的方式显示迭代的进度
 from tqdm.auto import tqdm
 
 # 超参数设置
@@ -55,17 +55,19 @@ hidden_dim = 256
 num_class = 2
 batch_size = 32
 num_epoch = 5
+filter_size = 3
+num_filter = 100
 
 # 加载数据
 train_data, test_data, vocab = load_sentence_polarity()
-train_dataset = BowDataset(train_data)
-test_dataset = BowDataset(test_data)
+train_dataset = CnnDataset(train_data)
+test_dataset = CnnDataset(test_data)
 train_data_loader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
 test_data_loader = DataLoader(test_dataset, batch_size=1, collate_fn=collate_fn, shuffle=False)
 
 # 加载模型
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = MLP(len(vocab), embedding_dim, hidden_dim, num_class)
+model = CNN(len(vocab), embedding_dim, filter_size, num_filter, num_class)
 model.to(device)  # 将模型加载到CPU或GPU设备
 
 # 训练过程
@@ -76,8 +78,8 @@ model.train()
 for epoch in range(num_epoch):
     total_loss = 0
     for batch in tqdm(train_data_loader, desc=f"Training Epoch {epoch}"):
-        inputs, offsets, targets = [x.to(device) for x in batch]
-        log_probs = model(inputs, offsets)
+        inputs, targets = [x.to(device) for x in batch]
+        log_probs = model(inputs)
         loss = nll_loss(log_probs, targets)
         optimizer.zero_grad()
         loss.backward()
@@ -88,9 +90,9 @@ for epoch in range(num_epoch):
 # 测试过程
 acc = 0
 for batch in tqdm(test_data_loader, desc=f"Testing"):
-    inputs, offsets, targets = [x.to(device) for x in batch]
+    inputs, targets = [x.to(device) for x in batch]
     with torch.no_grad():
-        output = model(inputs, offsets)
+        output = model(inputs)
         acc += (output.argmax(dim=1) == targets).sum().item()
 
 # 输出在测试集上的准确率
