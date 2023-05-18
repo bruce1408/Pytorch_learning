f44b167c5da6bd4551c5660cb296164c4489fcf5
bruce1408 <summer56567@163.com>
2023-05-18 16:08:05 +0800

    new code update

--- Pocket_Lab/resnet50_calc_param.py
+++ Pocket_Lab/resnet50_calc_param.py
@@ -8,6 +8,17 @@ else:
     summary(net, (3, 224, 224))
 
 
+def resnet_calc_param():
+    from torchvision.models import resnet50
+    from thop import profile
+    import torch
+    model = resnet50(pretrained=False)
+    input = torch.randn(1, 3, 224, 224)
+    macs, params = profile(model, inputs=(input, ))
+    print(macs)
+    print(params)
+
+
 print(net.parameters())
 print(net)
 # print(net.state_dict())
--- Quantity/resnet50_1.py
+++
@@ -1,8 +0,0 @@
-from torchvision.models import resnet50
-from thop import profile
-import torch
-model = resnet50(pretrained=False)
-input = torch.randn(1, 3, 224, 224)
-macs, params = profile(model, inputs=(input, ))
-print(macs)
-print(params)
