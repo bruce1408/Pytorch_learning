9df5331b6234c9671a1c618d8948986076dc86e1
bruce1408 <summer56567@163.com>
2023-05-18 19:28:49 +0800

    quant implement

--- Quantity/1_quant_implement_example.py
+++ Quantity/1_quant_implement_example.py
@@ -13,8 +13,9 @@ def quantize_simulation():
         
         weights_q1 = np.round(weights / scale_1)
         weights_q2 = np.round(weights / scale_2 + zero_point)
-        
-        # print(weights_q)
+        print(weights[0:5])
+        print(weights_q1[0:5])
+        print(weights_q2[0:5])
         weights_q1 = np.clip(weights_q1, qmin, qmax).astype(np.int8)
         weights_q2 = np.clip(weights_q2, qmin, qmax).astype(np.int8)
         
@@ -30,7 +31,7 @@ def quantize_simulation():
 
 
     # 加载原始模型权重
-    weights = np.random.randn(10000) * 100
+    weights = np.random.randn(1000) * 100
 
     # 定义量化位数
     num_bits = 8
@@ -38,7 +39,7 @@ def quantize_simulation():
     # 量化权重
     weights_q1, weights_q2, scale1, scale2, zero_point = quantize(weights, num_bits=num_bits)
 
-
+    print("scale1 : {} and scale2: {} offset: {} ".format( scale1, scale2, zero_point))
     # 将量化后的权重恢复为32位浮点数
     weights_deq1 = dequantize(weights_q1, scale1, None)
     weights_deq2 = dequantize(weights_q2, scale2, zero_point)
@@ -50,7 +51,7 @@ def quantize_simulation():
     # 输出量化误差和压缩比
     print('Weight1 quantization error:', weight_error1)
     print('Weight2 quantization error:', weight_error2)
-    # print('Weight compression ratio:', weights.size / weights_q.size)
+    # print('Weight compression ratio:', weights.size / weights_q1.size)
 
 
 quantize_simulation()
