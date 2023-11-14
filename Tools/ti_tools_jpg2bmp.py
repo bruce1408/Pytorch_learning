f6cf4d9bd5b541d0e8ece5f1e5f47ad26fb51fdf
bruce1408 <summer56557@163.com>
2023-11-14 11:34:11 +0800

    feat(py files): added format print log
    添加了在终端彩色格式化输出的形式

--- Tools/ti_tools_jpg2bmp.py
+++ Tools/ti_tools_jpg2bmp.py
@@ -5,8 +5,9 @@ from shutil import copy
 from PIL import Image
 import cv2
 import numpy as np
+from termcolor import cprint
 
-
+        
 def jpgToBmp(src_img_path, dst_img_path, nest_dir=False):
     src_img_path_list = []
     if nest_dir:
@@ -78,6 +79,20 @@ def generate_imagenet_val_label(img_dir):
             f.write(key + ":" + str(value) + "\n")
 
 
+
+
+def print_info(info, _type=None):
+    if _type is not None:
+        if isinstance(info, str):
+            cprint(info, _type[0], attrs=[_type[1]])
+        elif isinstance(info, list):
+            for i in range(info):
+                cprint(i, _type[0], attrs=[_type[1]])
+    else:
+        print(info)
+        
+        
+        
 def compare_res_with_evm_ti(img_txt_evm_res, img_txt_std_res):
     
     total_pred_num = 0
@@ -99,7 +114,18 @@ def compare_res_with_evm_ti(img_txt_evm_res, img_txt_std_res):
                 total_pred_num += 1
             total_num += 1
 
-    print("the total img nums is %d , the right predict num is %d, acc is: %.4f "%(total_num, total_pred_num, total_pred_num / total_num ))
+    print_info('------------------------------------------------------------------------------\n'
+           '|                    ImageNet Dataset Evaluation Results                      |\n'
+           '|                                                                             |\n'
+           
+           f'| the total img nums is {total_num}, the right predict num is {total_pred_num}, acc is: {total_pred_num / total_num:.3}    |\n'
+
+        #    'the total img nums is {}, the right predict num is {}, acc is: {} \n'.format(total_num, total_pred_num, total_pred_num / total_num )
+           '-----------------------------------------------------------------------------', ['yellow', 'bold'])
+
+    # print("the total img nums is %d , the right predict num is %d, acc is: %.4f "%(total_num, total_pred_num, total_pred_num / total_num ))
+    # print( 'the total img nums is {}, the right predict num is {}, acc is: {:.3} \n'.format(total_num, total_pred_num, total_pred_num / total_num ))
+
     
 
 def jpeg_to_bmp(jpeg_path, bmp_path):
@@ -142,52 +168,52 @@ if __name__ == '__main__':
     # generate_imagenet_val_label(val_dir_path)
     
     # 根据板卡上面的结果进行验证
-    img_txt_std_res = "/Users/bruce/PycharmProjects/Pytorch_learning/Tools/val_imagenet_label.txt"
-    img_txt_evm_res = "/Users/bruce/Downloads/15_Ti_model_files/imagenet_res_resnet34_post1.txt"
-    # compare_res_with_evm_ti(img_txt_evm_res, img_txt_std_res)
+    img_txt_std_res = "/home/bruce/Documents/Pytorch_learning/Tools/val_imagenet_label.txt"
+    img_txt_evm_res = "/home/bruce/Downloads/imagenet_res_custom_resnet34.txt"
+    compare_res_with_evm_ti(img_txt_evm_res, img_txt_std_res)
     
     
     # 图片解析
-    path_image = "/Users/bruce/Downloads/15_Ti_model_files/ILSVRC2012_val_00044292.bmp"
-    img_data = parse_image(path_image)
-    # print(img_data.shape)
+    # path_image = "/Users/bruce/Downloads/15_Ti_model_files/ILSVRC2012_val_00044292.bmp"
+    # img_data = parse_image(path_image)
+    # # print(img_data.shape)
     
-    image_chw = np.transpose(img_data, (2, 0, 1))
-    # print(image_chw.shape)
-    # print(image_chw[0].shape)
+    # image_chw = np.transpose(img_data, (2, 0, 1))
+    # # print(image_chw.shape)
+    # # print(image_chw[0].shape)
 
-    h, w = image_chw[0].shape
-    print(h, w)
-    print(image_chw[0][769][479])
-    print(img_data[:][769][479])
-    with open("image_data_00044292_R_python.txt", "w") as f:
-        for i in range(h):
-            for j in range(w):
-                f.write(str(image_chw[2][i][j])+"\n")
+    # h, w = image_chw[0].shape
+    # print(h, w)
+    # print(image_chw[0][769][479])
+    # print(img_data[:][769][479])
+    # with open("image_data_00044292_R_python.txt", "w") as f:
+    #     for i in range(h):
+    #         for j in range(w):
+    #             f.write(str(image_chw[2][i][j])+"\n")
                 
     
-    from PIL import Image
-    import numpy as np
+    # from PIL import Image
+    # import numpy as np
 
-    # 定义图片的宽度和高度
-    width = 480
-    height = 770
+    # # 定义图片的宽度和高度
+    # width = 480
+    # height = 770
 
-    # 打开图片文件
-    image = Image.open("/Users/bruce/Downloads/15_Ti_model_files/ILSVRC2012_val_00044292.bmp")
+    # # 打开图片文件
+    # image = Image.open("/Users/bruce/Downloads/15_Ti_model_files/ILSVRC2012_val_00044292.bmp")
 
-    # 读取图片数据
-    image_data = np.array(image)
+    # # 读取图片数据
+    # image_data = np.array(image)
 
-    # 输出图片数据到控制台
-    for i in range(height):
-        for j in range(width):
-            pixel = image_data[i, j]
-            print(f"Pixel ({j}, {i}): {pixel[0]}, {pixel[1]}, {pixel[2]}")
+    # # 输出图片数据到控制台
+    # for i in range(height):
+    #     for j in range(width):
+    #         pixel = image_data[i, j]
+    #         print(f"Pixel ({j}, {i}): {pixel[0]}, {pixel[1]}, {pixel[2]}")
 
-    # 输出图片数据到文本文件
-    with open("/Users/bruce/CppProjects/CPlusPlusThings/extensions/Opencv_learning/1_1_demo_read_img/image_data_1.txt", 'w') as file:
-        for i in range(height):
-            for j in range(width):
-                file.write(f"{image_data[i, j][0]}\n")
+    # # 输出图片数据到文本文件
+    # with open("/Users/bruce/CppProjects/CPlusPlusThings/extensions/Opencv_learning/1_1_demo_read_img/image_data_1.txt", 'w') as file:
+    #     for i in range(height):
+    #         for j in range(width):
+    #             file.write(f"{image_data[i, j][0]}\n")
 
