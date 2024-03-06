985e18edbafb41e34717a3375498b2371ff37d67
bruce1408 <summer56567@163.com>
2024-03-06 19:27:17 +0800

    🐞 fix(修改了 printk 包的名字): 修改了包的名字，让安装和包名一致

--- Tools/onnx_model_convert_onnxSimplify.py
+++ Tools/onnx_model_convert_onnxSimplify.py
@@ -55,8 +55,8 @@ def print_onnx_input_output(model_path):
         print("Shape:", shape, "Type:", onnx.TensorProto.DataType.Name(data_type))
 
 if __name__=="__main__":
-    model_path = "/Users/bruce/Downloads/8620_deploy/swin_tiny_patch4_window7_224_224_elementwise_affine.onnx"
-    # model_path = "/Users/bruce/Downloads/8620_deploy/Laneline/models/epoch_latest_0302.onnx"
+    # model_path = "/Users/bruce/Downloads/8620_deploy/swin_tiny_patch4_window7_224_224_elementwise_affine.onnx"
+    model_path = "/Users/bruce/Downloads/8620_deploy/Laneline/models/epoch_latest_0304.onnx"
     
     onnx_simplify(model_path)
     # print_onnx_input_output(model_path)
